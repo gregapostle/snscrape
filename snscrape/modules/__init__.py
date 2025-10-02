52f0ef5ee1cfba4cde3aa9eb8932766f98b1c57c
gregapostle <gapostle@techmatesgroup.com>
2025-10-02 15:09:26 -0400

    Fix Python 3.12 import error in _import_modules

--- snscrape/modules/__init__.py
+++ snscrape/modules/__init__.py
@@ -5,13 +5,17 @@ __all__ = []
 
 
 def _import_modules():
-	prefixLen = len(__name__) + 1
-	for importer, moduleName, isPkg in pkgutil.iter_modules(__path__, prefix = f'{__name__}.'):
-		assert not isPkg
-		moduleNameWithoutPrefix = moduleName[prefixLen:]
-		__all__.append(moduleNameWithoutPrefix)
-		module = importer.find_module(moduleName).load_module(moduleName)
-		globals()[moduleNameWithoutPrefix] = module
+    import importlib
+    import importlib.util
+    import pkgutil
+
+    for finder, moduleName, ispkg in pkgutil.iter_modules(__path__):
+        spec = importlib.util.find_spec(f'{__name__}.{moduleName}')
+        if spec is None:
+            continue
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        globals()[moduleName] = module
 
 
 _import_modules()
